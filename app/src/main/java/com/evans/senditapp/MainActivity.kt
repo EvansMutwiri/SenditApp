afaf29d28fa8472b6b605968fa2e5d3c4455632f
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-10-10 05:01:20 +0300

    user auth screens navigation

--- app/src/main/java/com/evans/senditapp/MainActivity.kt
+++ app/src/main/java/com/evans/senditapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.evans.senditapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+
+        //navigation controller
+        val navController = navHostFragment.navController
+//        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
